e89acb15c224e2f5645cf86363a729b41996fe64
Vincent Esche <regexident@gmail.com>
2023-07-26 14:01:41 +0200

    Add `struct StartBound<T, D>` & `struct EndBound<T, D>`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod bound;
 mod bounded;
 mod discreteness;
+mod end_bound;
+mod start_bound;
 
-pub use self::{bound::*, bounded::*, discreteness::*};
+pub use self::{bound::*, bounded::*, discreteness::*, end_bound::*, start_bound::*};
 
 mod sealed {
     pub trait Sealed {}
---
+++ src/start_bound.rs
@@ -0,0 +1,302 @@
+use std::{
+    cmp::Ordering,
+    hash::{Hash, Hasher},
+    marker::PhantomData,
+};
+
+use crate::{Bound, Bounded, Discreteness, EndBound};
+
+#[derive(Clone, Copy, Debug)]
+pub struct StartBound<T, D> {
+    pub(crate) bound: Bound<T>,
+    _phantom: PhantomData<D>,
+}
+
+impl<T, D> StartBound<T, D>
+where
+    D: Discreteness,
+{
+    pub fn bounded(value: T) -> Self {
+        Self::from(Bound::Bounded(value))
+    }
+
+    pub fn unbounded() -> Self {
+        Self::from(Bound::Unbounded)
+    }
+
+    pub fn bound(&self) -> &Bound<T> {
+        &self.bound
+    }
+
+    pub fn into_bound(self) -> Bound<T> {
+        self.bound
+    }
+}
+
+impl<T, D> From<Bound<T>> for StartBound<T, D>
+where
+    D: Discreteness,
+{
+    fn from(value: Bound<T>) -> Self {
+        Self {
+            bound: value,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, D> Hash for StartBound<T, D>
+where
+    T: Hash,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.bound.hash(state);
+    }
+}
+
+impl<T, D> Eq for StartBound<T, D>
+where
+    T: PartialEq<T>,
+    D: Discreteness,
+{
+}
+
+impl<T, D> PartialEq<StartBound<T, D>> for StartBound<T, D>
+where
+    T: PartialEq<T>,
+    D: Discreteness,
+{
+    fn eq(&self, other: &StartBound<T, D>) -> bool {
+        self.bound == other.bound
+    }
+}
+
+impl<T, D> PartialEq<EndBound<T, D>> for StartBound<T, D>
+where
+    T: PartialEq<T> + Bounded,
+    D: Discreteness,
+{
+    fn eq(&self, other: &EndBound<T, D>) -> bool {
+        use Bound::*;
+
+        match (self.bound(), other.bound()) {
+            (Bounded(s), Bounded(t)) => s.eq(t),
+            (Bounded(s), Unbounded) => s.eq(&T::max_value()),
+            (Unbounded, Bounded(t)) => T::min_value().eq(t),
+            (Unbounded, Unbounded) => false,
+        }
+    }
+}
+
+impl<T, D> PartialOrd<StartBound<T, D>> for StartBound<T, D>
+where
+    T: PartialOrd<T> + Bounded,
+    D: Discreteness,
+{
+    fn partial_cmp(&self, other: &StartBound<T, D>) -> Option<Ordering> {
+        use Bound::*;
+
+        match (self.bound(), other.bound()) {
+            (Bounded(s), Bounded(t)) => s.partial_cmp(t),
+            (Bounded(s), Unbounded) => s.partial_cmp(&T::min_value()),
+            (Unbounded, Bounded(t)) => T::min_value().partial_cmp(t),
+            (Unbounded, Unbounded) => Some(Ordering::Equal),
+        }
+    }
+}
+
+impl<T, D> PartialOrd<EndBound<T, D>> for StartBound<T, D>
+where
+    T: PartialOrd<T> + Bounded,
+    D: Discreteness,
+{
+    fn partial_cmp(&self, other: &EndBound<T, D>) -> Option<Ordering> {
+        use Bound::*;
+
+        match (self.bound(), other.bound()) {
+            (Bounded(s), Bounded(t)) => s.partial_cmp(t),
+            (Bounded(s), Unbounded) => s.partial_cmp(&T::max_value()),
+            (Unbounded, Bounded(t)) => T::min_value().partial_cmp(t),
+            (Unbounded, Unbounded) => Some(Ordering::Less),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use Ordering::*;
+
+    type Bnd = crate::Discrete;
+    // type Bnd = crate::NonDiscrete;
+
+    type Lower = StartBound<isize, Bnd>;
+    type Upper = EndBound<isize, Bnd>;
+
+    type Lhs = Lower;
+
+    mod vs_start_bound {
+        use super::*;
+
+        type Rhs = Lower;
+
+        #[test]
+        fn bounded_bounded() {
+            fn ordering(s: isize, t: isize) -> Option<Ordering> {
+                Lhs::bounded(s).partial_cmp(&Rhs::bounded(t))
+            }
+
+            assert_eq!(ordering(1, 1), Some(Equal));
+            assert_eq!(ordering(1, 2), Some(Less));
+            assert_eq!(ordering(1, 3), Some(Less));
+
+            assert_eq!(ordering(2, 1), Some(Greater));
+            assert_eq!(ordering(2, 2), Some(Equal));
+            assert_eq!(ordering(2, 3), Some(Less));
+
+            assert_eq!(ordering(3, 1), Some(Greater));
+            assert_eq!(ordering(3, 2), Some(Greater));
+            assert_eq!(ordering(3, 3), Some(Equal));
+
+            assert_eq!(Lhs::bounded(1), Rhs::bounded(1));
+            assert_ne!(Lhs::bounded(1), Rhs::bounded(2));
+            assert_ne!(Lhs::bounded(1), Rhs::bounded(3));
+
+            assert_ne!(Lhs::bounded(2), Rhs::bounded(1));
+            assert_eq!(Lhs::bounded(2), Rhs::bounded(2));
+            assert_ne!(Lhs::bounded(2), Rhs::bounded(3));
+
+            assert_ne!(Lhs::bounded(3), Rhs::bounded(1));
+            assert_ne!(Lhs::bounded(3), Rhs::bounded(2));
+            assert_eq!(Lhs::bounded(3), Rhs::bounded(3));
+        }
+
+        #[test]
+        fn bounded_unbounded() {
+            fn ordering(s: isize) -> Option<Ordering> {
+                Lhs::bounded(s).partial_cmp(&Rhs::unbounded())
+            }
+
+            assert_eq!(ordering(isize::MIN), Some(Equal));
+            assert_eq!(ordering(1), Some(Greater));
+            assert_eq!(ordering(2), Some(Greater));
+            assert_eq!(ordering(3), Some(Greater));
+            assert_eq!(ordering(isize::MAX), Some(Greater));
+
+            assert_ne!(Lhs::bounded(isize::MIN), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(1), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(2), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(3), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(isize::MAX), Rhs::unbounded());
+        }
+
+        #[test]
+        fn unbounded_bounded() {
+            fn ordering(t: isize) -> Option<Ordering> {
+                Lhs::unbounded().partial_cmp(&Rhs::bounded(t))
+            }
+
+            assert_eq!(ordering(isize::MIN), Some(Equal));
+            assert_eq!(ordering(1), Some(Less));
+            assert_eq!(ordering(2), Some(Less));
+            assert_eq!(ordering(3), Some(Less));
+            assert_eq!(ordering(isize::MAX), Some(Less));
+
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(isize::MIN));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(1));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(2));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(3));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(isize::MAX));
+        }
+
+        #[test]
+        fn unbounded_unbounded() {
+            assert_eq!(Lhs::unbounded().partial_cmp(&Rhs::unbounded()), Some(Equal));
+
+            assert_eq!(Lhs::unbounded(), Rhs::unbounded());
+        }
+    }
+
+    mod vs_end_bound {
+        use super::*;
+
+        type Rhs = Upper;
+
+        #[test]
+        fn bounded_bounded() {
+            fn ordering(s: isize, t: isize) -> Option<Ordering> {
+                Lhs::bounded(s).partial_cmp(&Rhs::bounded(t))
+            }
+
+            assert_eq!(ordering(1, 1), Some(Equal));
+            assert_eq!(ordering(1, 2), Some(Less));
+            assert_eq!(ordering(1, 3), Some(Less));
+
+            assert_eq!(ordering(2, 1), Some(Greater));
+            assert_eq!(ordering(2, 2), Some(Equal));
+            assert_eq!(ordering(2, 3), Some(Less));
+
+            assert_eq!(ordering(3, 1), Some(Greater));
+            assert_eq!(ordering(3, 2), Some(Greater));
+            assert_eq!(ordering(3, 3), Some(Equal));
+
+            assert_eq!(Lhs::bounded(1), Rhs::bounded(1));
+            assert_ne!(Lhs::bounded(1), Rhs::bounded(2));
+            assert_ne!(Lhs::bounded(1), Rhs::bounded(3));
+
+            assert_ne!(Lhs::bounded(2), Rhs::bounded(1));
+            assert_eq!(Lhs::bounded(2), Rhs::bounded(2));
+            assert_ne!(Lhs::bounded(2), Rhs::bounded(3));
+
+            assert_ne!(Lhs::bounded(3), Rhs::bounded(1));
+            assert_ne!(Lhs::bounded(3), Rhs::bounded(2));
+            assert_eq!(Lhs::bounded(3), Rhs::bounded(3));
+        }
+
+        #[test]
+        fn bounded_unbounded() {
+            fn ordering(s: isize) -> Option<Ordering> {
+                Lhs::bounded(s).partial_cmp(&Rhs::unbounded())
+            }
+
+            assert_eq!(ordering(isize::MIN), Some(Less));
+            assert_eq!(ordering(1), Some(Less));
+            assert_eq!(ordering(2), Some(Less));
+            assert_eq!(ordering(3), Some(Less));
+            assert_eq!(ordering(isize::MAX), Some(Equal));
+
+            assert_ne!(Lhs::bounded(isize::MIN), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(1), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(2), Rhs::unbounded());
+            assert_ne!(Lhs::bounded(3), Rhs::unbounded());
+            assert_eq!(Lhs::bounded(isize::MAX), Rhs::unbounded());
+        }
+
+        #[test]
+        fn unbounded_bounded() {
+            fn ordering(t: isize) -> Option<Ordering> {
+                Lhs::unbounded().partial_cmp(&Rhs::bounded(t))
+            }
+
+            assert_eq!(ordering(isize::MIN), Some(Equal));
+            assert_eq!(ordering(1), Some(Less));
+            assert_eq!(ordering(2), Some(Less));
+            assert_eq!(ordering(3), Some(Less));
+            assert_eq!(ordering(isize::MAX), Some(Less));
+
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(isize::MIN));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(1));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(2));
+            assert_ne!(Rhs::unbounded(), Lhs::bounded(3));
+            assert_eq!(Rhs::unbounded(), Lhs::bounded(isize::MAX));
+        }
+
+        #[test]
+        fn unbounded_unbounded() {
+            assert_eq!(Lhs::unbounded().partial_cmp(&Rhs::unbounded()), Some(Less));
+
+            assert_ne!(Lhs::unbounded(), Rhs::unbounded());
+        }
+    }
+}
