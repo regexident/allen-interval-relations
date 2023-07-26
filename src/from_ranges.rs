a861cead56db62874f9540ea6b43ddba7b997689
Vincent Esche <regexident@gmail.com>
2023-07-26 15:44:13 +0200

    Remove obsolete files

--- src/from_ranges.rs
+++ src/from_ranges.rs
@@ -1,3 +1,5 @@
+/// Create a value from a pair of ranges.
 pub trait FromRanges<S, T>: Sized {
+    /// Creates a value from a pair of ranges.
     fn from_ranges(s: S, t: T) -> Option<Self>;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -41,21 +41,8 @@
 #![warn(missing_docs)]
 
 mod atomics;
-mod bound;
 mod bounded;
-mod discreteness;
-mod end_bound;
 mod from_ranges;
-mod range_bounds;
-mod range_bounds_ext;
 mod relation;
-mod start_bound;
 
-pub use self::{
-    atomics::*, bound::*, bounded::*, discreteness::*, end_bound::*, from_ranges::*,
-    range_bounds::*, range_bounds_ext::*, relation::*, start_bound::*,
-};
-
-mod sealed {
-    pub trait Sealed {}
-}
+pub use self::{atomics::*, bounded::*, from_ranges::*, relation::*};
