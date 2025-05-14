2db4b8b36329812b8a7be33dea2c256c6709ae0e
Vincent Esche <regexident@gmail.com>
2025-05-14 13:48:46 +0200

    Add support for `thiserror` (gated behind a default-enabled feature)

--- src/interval.rs
+++ src/interval.rs
@@ -7,10 +7,13 @@ mod interval_to;
 pub use self::{interval::*, interval_from::*, interval_full::*, interval_to::*};
 
 /// Error type specific to Allen's interval algebra.
+#[cfg_attr(feature = "std", derive(thiserror::Error))]
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub enum IntervalError {
     /// Empty intervals are invalid with respect to Allen's interval algebra.
+    #[error("empty interval, which is not supported by Allen's interval algebra")]
     EmptyInterval,
     /// Could not obtain total order.
+    #[error("could not obtain total order")]
     AmbiguousOrder,
 }
